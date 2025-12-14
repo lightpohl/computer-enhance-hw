e229e3960ba2b9b9e7f58a367f22c367e70979f8
Joshua Pohl <joshuapohl@gmail.com>
2025-12-14 10:23:26 -0700

    prepare for extended mov support

--- 8086_decode/decode.py
+++ 8086_decode/decode.py
@@ -1,4 +1,5 @@
 import sys
+from typing import BinaryIO, Optional
 
 REG_LOOKUP = {
     0b0000: "al",
@@ -19,23 +20,30 @@ REG_LOOKUP = {
     0b1111: "di",
 }
 
+INSTRUCTION_TYPE = {
+    "MOV_REG_TO_REG": "100010",
+    "MOV_REG_IMM": "1011",
+}
+
+INTRUCTION_TYPE_TO_TEXT = {
+    "MOV_REG_TO_REG": "mov",
+}
+
 
 def get_operation(chunk: bytes) -> str:
     byte = chunk[0]
-    op_code = byte >> 2
+    binary_string = f"{byte:08b}"
 
-    if op_code == int("100010", 2):
-        return "mov"
+    if binary_string.startswith(INSTRUCTION_TYPE["MOV_REG_TO_REG"]):
+        return INTRUCTION_TYPE_TO_TEXT["MOV_REG_TO_REG"]
+    elif binary_string.startswith(INSTRUCTION_TYPE["MOV_REG_IMM"]):
+        return INTRUCTION_TYPE_TO_TEXT["MOV_REG_IMM"]
     else:
-        raise Exception("unsupported op_code")
+        raise Exception(f"unsupported op_code: 0x{byte:02x} ({binary_string})")
 
 
-def get_d_bit(chunk: bytes) -> int:
-    return (chunk[0] >> 1) & 1
-
-
-def get_w_bit(chunk: bytes) -> int:
-    return chunk[0] & 1
+def get_mod(byte: int) -> int:
+    return (byte >> 6) & 3
 
 
 def get_reg(d_bit: int, w_bit: int, destination: bool, byte: int) -> str:
@@ -47,14 +55,36 @@ def get_reg(d_bit: int, w_bit: int, destination: bool, byte: int) -> str:
     return REG_LOOKUP[key]
 
 
-def get_operands(chunk: bytes) -> str:
-    d_bit = get_d_bit(chunk)
-    w_bit = get_w_bit(chunk)
+def get_operands(chunk: bytes, operation: str) -> str:
+    if operation == INTRUCTION_TYPE_TO_TEXT["MOV_REG_TO_REG"]:
+        d_bit = (chunk[0] >> 1) & 1
+        w_bit = chunk[0] & 1
 
-    dst = get_reg(d_bit, w_bit, True, chunk[1])
-    src = get_reg(d_bit, w_bit, False, chunk[1])
+        dst = get_reg(d_bit, w_bit, True, chunk[1])
+        src = get_reg(d_bit, w_bit, False, chunk[1])
 
-    return f"{dst}, {src}"
+        return f"{dst}, {src}"
+
+    raise Exception("unsupported operation")
+
+
+def read_additional_chunks(file: BinaryIO, operation: str) -> Optional[bytes]:
+    additional_chunk = None
+    if operation == INTRUCTION_TYPE_TO_TEXT["MOV_REG_TO_REG"]:
+        additional_chunk = file.read(1)
+        mod = get_mod(additional_chunk[0])
+        if mod == 0b11:
+            pass
+        elif mod == 0b00:
+            r_m = additional_chunk[0] & 0b111
+            if r_m == 0b110:
+                additional_chunk += file.read(2)
+        elif mod == 0b01:
+            additional_chunk += file.read(1)
+        elif mod == 0b10:
+            additional_chunk += file.read(2)
+
+    return additional_chunk
 
 
 def main():
@@ -69,13 +99,18 @@ def main():
         print("bits 16")
 
         while True:
-            chunk = file.read(2)
+            chunk = file.read(1)
 
             if not chunk:
                 break
 
             operation = get_operation(chunk)
-            operands = get_operands(chunk)
+            additional_chunk = read_additional_chunks(file, operation)
+
+            if additional_chunk:
+                chunk += additional_chunk
+
+            operands = get_operands(chunk, operation)
 
             print(f"{operation} {operands}")
 
